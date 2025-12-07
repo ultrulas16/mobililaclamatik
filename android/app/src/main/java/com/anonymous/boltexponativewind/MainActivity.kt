1b07f66677be19a43751272129b7ed59cb9e2d9d
ultrulas16 <ulaserbetcioglu@gmail.com>
2025-12-07 15:04:02 +0300

    Updated Podfile.properties.json

--- android/app/src/main/java/com/anonymous/boltexponativewind/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/boltexponativewind/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.boltexponativewind
+package com.anonymous.boltexponativewind
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/boltexponativewind/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/boltexponativewind/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.boltexponativewind
+package com.anonymous.boltexponativewind
 
 import android.app.Application
 import android.content.res.Configuration
